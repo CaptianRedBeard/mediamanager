631fa7ccc9f8322e920608bae403c4ade696221d
CaptianRedBeard <taggartandrewjames@gmail.com>
2025-07-27 16:39:16 -0400

    added imageservice wrappers

--- backend/imageservice/service.go
+++ backend/imageservice/service.go
@@ -3,13 +3,17 @@ package imageservice
 type ImageService struct {
 	Blobs    *BlobService
 	Meta     *MetadataService
+	Tags     *TagService
+	Albums   *AlbumService
 	Importer *Importer
 }
 
-func New(blobs *BlobService, meta *MetadataService) *ImageService {
+func New(blobs *BlobService, meta *MetadataService, tags *TagService, albums *AlbumService) *ImageService {
 	return &ImageService{
 		Blobs:    blobs,
 		Meta:     meta,
+		Tags:     tags,
+		Albums:   albums,
 		Importer: NewImporter(blobs, meta),
 	}
 }
